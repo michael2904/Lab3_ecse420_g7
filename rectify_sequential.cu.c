7598222cc862f788b1acfdfa88db517fe23aa2e2
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-21 17:52:02 -0500

    first version of rectify to test

--- rectify_sequential.cu.c
+++ rectify_sequential.cu.c
@@ -3,42 +3,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BLOCK_WIDTH 1000
+
+
+__global__ void rectify(float * d_out, float * d_in){
+	int idx = threadIdx.x;
+	float f = d_in[idx];
+	if(idx % 4 == 3){
+		f = f < 127 ? 127 : f; // R
+	}
+	d_out[idx] = f;
+}
+
+
 void process(char* input_filename, char* output_filename)
 {
-  unsigned error;
-  unsigned char *image, *new_image;
-  unsigned width, height;
+	unsigned error;
+	unsigned char *image, *new_image;
+	unsigned width, height;
+	const int size = width * height * 4 * sizeof(unsigned char);
 
-  error = lodepng_decode32_file(&image, &width, &height, input_filename);
-  if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
-  new_image = malloc(width * height * 4 * sizeof(unsigned char));
+	//image --> h_in
+	//new_image --> h_out
 
-  // rectify image
-  unsigned char value;
-  int i,j;
-  for (i = 0; i < height; i++) {
-    for (j = 0; j < width; j++) {
-      for (int k = 0; k < 1000; k++) {
-  	    new_image[4*width*i + 4*j + 0] = image[4*width*i + 4*j + 0] < 127 ? 127 : image[4*width*i + 4*j + 0]; // R
-  	    new_image[4*width*i + 4*j + 1] = image[4*width*i + 4*j + 1] < 127 ? 127 : image[4*width*i + 4*j + 1]; // G
-  	    new_image[4*width*i + 4*j + 2] = image[4*width*i + 4*j + 2] < 127 ? 127 : image[4*width*i + 4*j + 2]; // B
-  	    new_image[4*width*i + 4*j + 3] = image[4*width*i + 4*j + 3]; // A
-      }
-    }
-  }
+	error = lodepng_decode32_file(&image, &width, &height, input_filename);
+	if(error) printf("error %u: %s\n", error, lodepng_error_text(error));
+	new_image = malloc(size);
 
-  lodepng_encode32_file(output_filename, new_image, width, height);
 
-  free(image);
-  free(new_image);
+	// declare GPU memory pointers
+	float * d_in;
+	float * d_out;
+
+	// allocate GPU memory
+	cudaMalloc(&d_in, size);
+	cudaMalloc(&d_out, size);
+
+	// transfer the array to the GPU
+	cudaMemcpy(d_in, image, size, cudaMemcpyHostToDevice);
+
+	// launch the kernel
+	rectify<<<size/BLOCK_WIDTH, BLOCK_WIDTH>>>(d_out, d_in);
+
+	// copy back the result array to the CPU
+	cudaMemcpy(new_image, d_out, size, cudaMemcpyDeviceToHost);
+
+	// // rectify image
+	// unsigned char value;
+	// int i,j;
+	// for (i = 0; i < height; i++) {
+	// 	for (j = 0; j < width; j++) {
+	// 		for (int k = 0; k < 3; k++) {
+	// 			new_image[4*width*i + 4*j + k] = image[4*width*i + 4*j + k] < 127 ? 127 : image[4*width*i + 4*j + k]; // R
+	// 		}
+	// 		new_image[4*width*i + 4*j + 3] = image[4*width*i + 4*j + 3]; // A
+	// 	}
+	// }
+
+	cudaFree(d_in);
+	cudaFree(d_out);
+
+	lodepng_encode32_file(output_filename, new_image, width, height);
+
+	free(image);
+	free(new_image);
 }
 
 int main(int argc, char *argv[])
 {
-  char* input_filename = argv[1];
-  char* output_filename = argv[2];
+	char* input_filename = argv[1];
+	char* output_filename = argv[2];
 
-  process(input_filename, output_filename);
+	process(input_filename, output_filename);
 
-  return 0;
+	return 0;
 }
