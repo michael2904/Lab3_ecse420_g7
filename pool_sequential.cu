ca72aca1969eebe38f55b50a99be585f90d95e51
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-22 16:50:40 -0500

    test pool

--- pool_sequential.cu
+++ pool_sequential.cu
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BLOCK_WIDTH 1024
+#define BLOCK_WIDTH 512
 
 //Putting blocks of size width divided by 0, so that each thread can access the neighboring values. There is no neighboring value that is called twice.
 
