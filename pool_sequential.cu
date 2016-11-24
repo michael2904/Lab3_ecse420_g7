a36689994da54b30306d042c72ab13a9c7b63db2
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-24 11:02:01 -0500

    testing stuff

--- pool_sequential.cu
+++ pool_sequential.cu
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BLOCK_WIDTH 128
+#define BLOCK_WIDTH 512
 
 //Putting blocks of size width divided by 0, so that each thread can access the neighboring values. There is no neighboring value that is called twice.
 
-__global__ void pool(int * d_out, unsigned char * d_in){
+__global__ void pool(int * d_out, unsigned char * d_in, int N){
 	int idx = threadIdx.x;
 	int jdx = threadIdx.y;
 	int kdx = threadIdx.z;
@@ -17,26 +17,31 @@ __global__ void pool(int * d_out, unsigned char * d_in){
 	int Bix = blockIdx.x;
 	int Biy = blockIdx.y;
 	int Biz = blockIdx.z;
-	int index = threadIdx.z * blockDim.x * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
-	int width = blockDim.x*blockDim.y*blockDim.z;
+	//int index = threadIdx.z * blockDim.x * blockDim.y + threadIdx.y * blockDim.x + threadIdx.x;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int index = idx + jdx * N;
+	if(index < 100){
+		printf("Dimensions are Bx:%d By:%d Bz:%d Index: %05d indexes are: Bix:%d Biy:%d Biz:%d -- Threads are Tx:%d Ty:%d Tz: %d -- coord (%d,%d,%d) col:%d, row:%d\n", Bx,By,Bz,index,Bix,Biy,Biz,threadIdx.x,threadIdx.y, threadIdx.z,kdx,jdx,idx,col,row);
+	}
 
 	//unsigned char max;
-	//int new_width = width/2;
-	//if(jdx%2 == 0 && kdx != 3){
-	//	max = d_in[4*width*idx + 4*jdx + kdx];
-	//	if(blockIdx.x == 0)printf("Original max = %d at (%d,%d,%d) for index = %d\n",max,idx,jdx,kdx,index);
-	//	if(d_in[4*width*(idx+1) + 4*jdx + kdx]>max) max = d_in[4*width*(idx+1) + 4*jdx + kdx];
-	//	if(d_in[4*width*(idx+1) + 4*(jdx+1) + kdx]>max) max = d_in[4*width*(idx+1) + 4*(jdx+1) + kdx];
-	//	if(d_in[4*width*idx + 4*(jdx+1) + kdx]>max) max = d_in[4*width*idx + 4*(jdx+1) + kdx];
-	//	d_out[new_width*idx + jdx*2 + kdx] = max;
-	//	if(blockIdx.x == 0)printf("Not max = %d and stored %d at %d, at (%d,%d,%d) for index = %d\n",max,d_out[new_width*idx + jdx*2 + kdx],new_width*idx + jdx*2 + kdx,idx,jdx,kdx,index);
-	//}
-	//if(jdx % 2 == 0 && kdx == 3){
-	//	d_out[new_width * idx + jdx*2 + 3] = d_in[4*width*idx + 4*jdx + 3];
-	//}
-	d_out[index] = index;
-	printf("Dimensions are Bx:%d By:%d Bz:%d Index: %05d indexes are: Bix:%d Biy:%d Biz:%d -- Threads are Tx:%d Ty:%d Tz: %d -- coord (%d,%d,%d)\n", Bx,By,Bz,index,Bix,Biy,Biz,threadIdx.x,threadIdx.y, threadIdx.z,kdx,jdx,idx);
-		printf("This is the index %d and this is d_out %d\n",index,d_out[index]);
+	// int new_width = width/2;
+	//     if(jdx%2 == 0 && kdx != 3){
+	//         max = d_in[4*width*idx + 4*jdx + kdx];
+	//         if(blockIdx.x == 0)printf("Original max = %d at (%d,%d,%d) for index = %d\n",max,idx,jdx,kdx,index);
+	//         if(d_in[4*width*(idx+1) + 4*jdx + kdx]>max) max = d_in[4*width*(idx+1) + 4*jdx + kdx];
+	//         if(d_in[4*width*(idx+1) + 4*(jdx+1) + kdx]>max) max = d_in[4*width*(idx+1) + 4*(jdx+1) + kdx];
+	//         if(d_in[4*width*idx + 4*(jdx+1) + kdx]>max) max = d_in[4*width*idx + 4*(jdx+1) + kdx];
+	//         d_out[new_width*idx + jdx*2 + kdx] = max;
+	//         if(blockIdx.x == 0)printf("Not max = %d and stored %d at %d, at (%d,%d,%d) for index = %d\n",max,d_out[new_width*idx + jdx*2 + kdx],new_width*idx + jdx*2 + kdx,idx,jdx,kdx,index);
+	//     }
+	//     if(jdx % 2 == 0 && kdx == 3){
+	//         d_out[new_width * idx + jdx*2 + 3] = d_in[4*width*idx + 4*jdx + 3];
+	//     }
+	//d_out[index] = index;
+	//printf("Dimensions are Bx:%d By:%d Bz:%d Index: %05d indexes are: Bix:%d Biy:%d Biz:%d -- Threads are Tx:%d Ty:%d Tz: %d -- coord (%d,%d,%d)\n", Bx,By,Bz,index,Bix,Biy,Biz,threadIdx.x,threadIdx.y, threadIdx.z,kdx,jdx,idx);
+	//printf("This is the index %d and this is d_out %d\n",index,d_out[index]);
 }
 
 
@@ -78,11 +83,11 @@ int process(char* input_filename, char* output_filename){
 	printf("%d total threads in %d blocks of size %d\n",size, block_quantity, BLOCK_WIDTH);
 
 	// launch the kernel
-	dim3 dimGrid(2, 2, 2);
-	dim3 dimBlock(1024, 2, 2);
+	dim3 dimGrid((width+(BLOCK_WIDTH-1))/BLOCK_WIDTH, (height+(BLOCK_WIDTH-1))/BLOCK_WIDTH);
+	dim3 dimBlock(width+BLOCK_WIDTH, BLOCK_WIDTH);
 
 
-	pool<<<dimGrid, dimBlock>>>(d_out, d_in);
+	pool<<<dimGrid, dimBlock>>>(d_out, d_in,width);
 
 	// copy back the result array to the CPU
 	cudaMemcpy(new_image, d_out, new_size, cudaMemcpyDeviceToHost);
