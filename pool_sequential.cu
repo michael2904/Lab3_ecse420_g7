8e239cc52c6cdaa38c128f28990d443f839a2228
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-27 18:12:24 -0500

    test pool

--- pool_sequential.cu
+++ pool_sequential.cu
@@ -18,23 +18,20 @@ __global__ void pool(int * d_out, unsigned char * d_in,int width){
 		printf("ind: %05d and width is %d : (%d,%d,%d)\n",ind,width,i,j,k);
 	}
 
-	// unsigned char max;
-	// int new_width = width/2;
-	// if(j%2 == 0 && k != 3){
-	//     max = d_in[4*width*i + 4*j + k];
-	//     if(blockIdx.x == 0)printf("Original max = %d at (%d,%d,%d) for ind = %d\n",max,i,j,k,ind);
-	//     if(d_in[4*width*(i+1) + 4*j + k]>max) max = d_in[4*width*(i+1) + 4*j + k];
-	//     if(d_in[4*width*(i+1) + 4*(j+1) + k]>max) max = d_in[4*width*(i+1) + 4*(j+1) + k];
-	//     if(d_in[4*width*i + 4*(j+1) + k]>max) max = d_in[4*width*i + 4*(j+1) + k];
-	    
-	//     d_out[new_width*i + j*2 + k] = max;
-	    
-	//     if(blockIdx.x == 0)printf("Not max = %d and stored %d at %d, at (%d,%d,%d) for ind = %d\n",max,d_out[new_width*i + j*2 + k],new_width*i + j*2 + k,i,j,k,ind);
-	// }
-	// if(j % 2 == 0 && k == 3){
-	//     d_out[new_width * i + j*2 + 3] = d_in[4*width*i + 4*j + 3];
-	// }
-
+	unsigned char max;
+	int new_width = width/2;
+	if(j%2 == 0 && k != 3){
+		max = d_in[4*width*i + 4*j + k];
+		if(d_in[4*width*(i+1) + 4*j + k]>max) max = d_in[4*width*(i+1) + 4*j + k];
+		if(d_in[4*width*(i+1) + 4*(j+1) + k]>max) max = d_in[4*width*(i+1) + 4*(j+1) + k];
+		if(d_in[4*width*i + 4*(j+1) + k]>max) max = d_in[4*width*i + 4*(j+1) + k];
+		d_out[new_width*i + j*2 + k] = max;
+		
+		if(j < 10)printf("Not max = %d and stored %d at %d, at (%d,%d,%d) for ind = %d\n",max,d_out[new_width*i + j*2 + k],new_width*i + j*2 + k,i,j,k,ind);
+	}
+	if(j % 2 == 0 && k == 3){
+		d_out[new_width * i + j*2 + 3] = d_in[4*width*i + 4*j + 3];
+	}
 }
 
 
