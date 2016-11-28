2811800ca50c84c3071496598491242a345e758d
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-28 00:06:29 -0500

    test convolve

--- convolve.cu
+++ convolve.cu
@@ -31,7 +31,7 @@ __global__ void convolve(unsigned char * d_out, unsigned char * d_in,int width,i
 			for (jj = 0; jj < 3; jj++) {
 				currentWF = w[ii][jj];
 				value += d_in[4*width*(i+ii-1) + 4*(j+jj-1) + k] * currentWF;
-				if(ind>3952120)printf("ind %d w(%d,%d)=%f|value : %f|\n",ii,jj,w[ii][jj],value);
+				if(ind>3952120)printf("ind %d w(%d,%d)=%f|value : %f|\n",ind,ii,jj,w[ii][jj],value);
 			}
 			if(ind>3952120)printf("\n");
 		}
