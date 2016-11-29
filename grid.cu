e1fcab9549a9e2e790c7ec1b0a74b0061802f527
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-28 22:20:05 -0500

    testing grid

--- grid.cu
+++ grid.cu
@@ -91,28 +91,28 @@ int process(int T){
 
 	int t;
 	for (t = 0; t < T; t++) {
-		printf("Run %d | %d total size with width %d and height %d in %d blocks of size %d. Size of memory %d\n",t,(N*N),N,N, ((N*N)+(BLOCK_WIDTH-1))/BLOCK_WIDTH, BLOCK_WIDTH,size);
-		printf("Try printing %f %f %f\n",u[ind(N/2,N/2)],u1[ind(N/2,N/2)],u2[ind(N/2,N/2)]);
-		for (i = (N/2)-5; i < (N/2)+5; i++) {
-			for (j = (N/2)-5; j < (N/2)+5; j++) {
-				printf("u(%d,%d) %f |",i,j,u[ind(i,j)]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-		for (i = (N/2)-5; i < (N/2)+5; i++) {
-			for (j = (N/2)-5; j < (N/2)+5; j++) {
-				printf("u1(%d,%d) %f |",i,j,u1[ind(i,j)]);
-			}
-			printf("\n");
-		}
-		printf("\n");
-		for (i = (N/2)-5; i < (N/2)+5; i++) {
-			for (j = (N/2)-5; j < (N/2)+5; j++) {
-				printf("u2(%d,%d) %f |",i,j,u2[ind(i,j)]);
-			}
-			printf("\n");
-		}
+		// printf("Run %d | %d total size with width %d and height %d in %d blocks of size %d. Size of memory %d\n",t,(N*N),N,N, ((N*N)+(BLOCK_WIDTH-1))/BLOCK_WIDTH, BLOCK_WIDTH,size);
+		// printf("Try printing %f %f %f\n",u[ind(N/2,N/2)],u1[ind(N/2,N/2)],u2[ind(N/2,N/2)]);
+		// for (i = (N/2)-5; i < (N/2)+5; i++) {
+		// 	for (j = (N/2)-5; j < (N/2)+5; j++) {
+		// 		printf("u(%d,%d) %f |",i,j,u[ind(i,j)]);
+		// 	}
+		// 	printf("\n");
+		// }
+		// printf("\n");
+		// for (i = (N/2)-5; i < (N/2)+5; i++) {
+		// 	for (j = (N/2)-5; j < (N/2)+5; j++) {
+		// 		printf("u1(%d,%d) %f |",i,j,u1[ind(i,j)]);
+		// 	}
+		// 	printf("\n");
+		// }
+		// printf("\n");
+		// for (i = (N/2)-5; i < (N/2)+5; i++) {
+		// 	for (j = (N/2)-5; j < (N/2)+5; j++) {
+		// 		printf("u2(%d,%d) %f |",i,j,u2[ind(i,j)]);
+		// 	}
+		// 	printf("\n");
+		// }
 
 
 		// transfer the array to the GPU
